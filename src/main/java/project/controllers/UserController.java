e52a319788c72449a9803f31f61656a017e172f3
Arman Mikayelovich <armanmikayelovich@gmail.com>
2019-02-06 23:45:15 +0400

    add new user method works...

--- src/main/java/project/controllers/UserController.java
+++ src/main/java/project/controllers/UserController.java
@@ -1,8 +1,6 @@
 package project.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,9 +17,8 @@ import project.exception.ResourceNotFoundException;
 import project.model.UserEntity;
 import project.repository.UserRepository;
 
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-import java.util.List;
+
 
 @RestController
 @RequestMapping("/api")
@@ -40,16 +37,15 @@ public class UserController {
         return modelAndView;
     }
     /////////
-
-    // Create a new User
-    @PostMapping(value = "/users", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
-            headers = "application/x-www-form-urlencoded;charset=UTF-8",
-            produces = {MediaType.APPLICATION_ATOM_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
-    public String createUser(@Valid @RequestBody UserEntity userEntity, HttpServletResponse response) throws Exception {
-
-        userRepository.save(userEntity);
-        return "post";
+//    @PostMapping(path = "/users",  consumes = { MediaType.APPLICATION_JSON_VALUE },
+//            headers = "application/x-www-form-urlencoded;charset=UTF-8",
+//            produces = "application/json")
+    @PostMapping("/users")
+    UserEntity newUser( UserEntity userEntity) {
+        System.out.println(userEntity);
+        return userRepository.save(userEntity);
     }
+
 
     // Get a Single User
     @GetMapping("/users/{id}")
--- src/main/java/project/model/UserEntity.java
+++ src/main/java/project/model/UserEntity.java
@@ -101,13 +101,12 @@ public class UserEntity {
 
     public UserEntity() { }
 
-    public UserEntity(String firstName, String lastName, String email, String phoneNumber, String password, List<ProductEntity> productList) {
+    public UserEntity(String firstName, String lastName, String email, String phoneNumber, String password) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.phoneNumber = phoneNumber;
         this.password = password;
-        this.productList = productList;
     }
 
     @Override
