35a7938fd4e8532a3d4afb191ca4070e45d307fb
araikgevorgian <arayik.gevorgyan@yandex.ru>
2019-02-12 23:24:36 +0400

    implementing item searching method

--- src/main/java/project/repository/ItemRepository.java
+++ src/main/java/project/repository/ItemRepository.java
@@ -2,11 +2,20 @@ package project.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import org.springframework.stereotype.Repository;
+
 import project.model.Item;
+
+import java.util.List;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
+    @Query("select s from Item s where s.searchTag like ?1")
+    static List<Item> findBySearchTag(String searchTag){
+        return null;
+    }
 }
 
