bbaea42892cf0e17d544440f5864388b9e7b155e
araikgevorgian <arayik.gevorgyan@yandex.ru>
2019-02-16 16:19:05 +0400

    0

--- src/main/java/project/Application.java
+++ src/main/java/project/Application.java
@@ -12,14 +12,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class Application {
-
-
+    
     public static void main(String[] args) {
 
         SpringApplication.run(Application.class, args);
     }
-
-
-
 
 }
