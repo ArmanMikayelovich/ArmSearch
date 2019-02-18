74e6cad61953890fa0097b48aac2fed384d2c165
Arman Mikayelovich <armanmikayelovich@gmail.com>
2019-02-18 20:43:06 +0400

    fix pom.xml

--- src/main/java/project/controllers/homePageController.java
+++ src/main/java/project/controllers/homePageController.java
@@ -15,7 +15,16 @@ public class homePageController {
     @GetMapping(value = {"/", "/home"})
     public ModelAndView getHomePage() {
         ModelAndView view = new ModelAndView("Home");
-        view.addObject("categoryGroup1",categoryGroupService.findByName("Electronics"))
+        view.addObject("electronicsGroup",categoryGroupService.findByName("Electronics"))
+        view.addObject("realEstateGroup",categoryGroupService.findByName("Electronics"))
+        view.addObject("vehiclesGroup",categoryGroupService.findByName("Electronics"))
+        view.addObject("servicesGroup",categoryGroupService.findByName("Electronics"))
+        view.addObject("jobsGroup",categoryGroupService.findByName("Electronics"))
+        view.addObject("fashionGroup",categoryGroupService.findByName("Electronics"))
+        view.addObject("toolsAndMaterialsGroup",categoryGroupService.findByName("Electronics"))
+        view.addObject("HouseholdGroup",categoryGroupService.findByName("Electronics"))
+        view.addObject("forKidsGroup",categoryGroupService.findByName("Electronics"))
+        view.addObject("Group",categoryGroupService.findByName("Electronics"))
     }
 
 }
